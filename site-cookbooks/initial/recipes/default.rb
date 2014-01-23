b3c0d5efcda21889aef36b67dcc378e6e4e23ef2
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-01-23 15:42:08 +0000

    some packages

--- site-cookbooks/initial/recipes/default.rb
+++ site-cookbooks/initial/recipes/default.rb
@@ -41,7 +41,18 @@ include_recipe "mysql::client"
 # there are too many incompatibilities between apache 2.2 and 2.4
 package "apache2"
 
-my_packages = %w{tmux ikiwiki libtext-markdown-perl libtext-multimarkdown-perl libhighlight-perl libxml-writer-perl}
+my_packages = %w{
+tmux ikiwiki libtext-markdown-perl libtext-multimarkdown-perl libhighlight-perl
+libxml-writer-perl postfix mutt-patched libgnutls-dev libfaad-dev libmad0-dev
+libao-dev openssh-server tig git gitk meld xclip xdotool xbindkeys jq colordiff
+wdiff sysstat atop iotop dstat htop nethogs exuberant-ctags mplayer2 youtube-dl
+aptitude compizconfig-settings-manager gnome-tweak-tool mplayer2
+transmission-gtk gtk2-engines-pixbuf indicator-multiload cabextract dconf-tools
+vim-gnome alarm-clock-applet lm-sensors hddtemp psensor pbzip2 tree
+openjdk-7-jdk ant hamster-indicator ccache sqlitebrowser lftp xapian-omega
+libsearch-xapian-perl libdigest-sha-perl libhtml-scrubber-perl
+}
+
 my_packages.each do |pkg|
   package pkg
 end
